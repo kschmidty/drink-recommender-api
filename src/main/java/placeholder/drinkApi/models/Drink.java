bca03a19108596c3d53b51fb5e10b326bcc6835c
kschmidty <developrprojects@gmail.com>
2019-07-28 23:26:13 -0400

    added ingredients to model

--- src/main/java/placeholder/drinkApi/models/Drink.java
+++ src/main/java/placeholder/drinkApi/models/Drink.java
@@ -5,6 +5,8 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.List;
+
 @Data
 @Document(collection = "drinks")
 public class Drink {
@@ -13,6 +15,8 @@ public class Drink {
     public String id;
 
     public String name;
+
+    public List<String> ingredients;
 
     public Drink(String name) {
         this.name = name;
